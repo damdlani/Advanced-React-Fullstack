6820525396a5b9ff97a6f94fa6f785d810b7ec3b
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2021-04-14 07:13:45 +0200

    change refetching queries to clearing the cache

--- frontend/components/DeleteProduct/DeleteProduct.js
+++ frontend/components/DeleteProduct/DeleteProduct.js
@@ -1,14 +1,25 @@
 import { useMutation } from "@apollo/client";
 import { DELETE_PRODUCT_MUTATION } from "./queries";
-import { ALL_PRODUCTS_QUERY } from "../Products/queries";
+// import { ALL_PRODUCTS_QUERY } from "../Products/queries"; only for refetching queries
 
 export const DeleteProduct = ({ id, children }) => {
-  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
-    variables: {
-      id,
-    },
-    refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
-  });
+  //keystone.js feature
+  const update = (cache, payload) => {
+    console.log(payload);
+    console.log("running the update function after delete");
+    cache.evict(cache.identify(payload.data.deleteProduct));
+  }
+
+  const [deleteProduct, { loading }] = useMutation(
+    DELETE_PRODUCT_MUTATION, 
+    {
+    variables: { id },
+    // refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],  
+    //sometimes refetching from network is not the best case
+    //deleting an item from apollo cache may be better
+    update,
+    }
+  );
 
   return (
     <button
