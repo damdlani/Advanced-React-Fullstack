5426753e9b869238b18dd06a1668af834ededa70
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2021-04-14 07:54:33 +0200

    feature: create pagination with working next/prev

---
+++ frontend/components/Pagination/Pagination.js
@@ -0,0 +1,33 @@
+import { useQuery } from "@apollo/client";
+import Head from "next/head";
+import Link from "next/link";
+import PaginationStyles from "../styles/PaginationStyles";
+import { PAGINATION_QUERY } from "./queries";
+import DisplayError from "../ErrorMessage";
+import { perPage } from "../../config";
+
+export const Pagination = ({ page }) => {
+  const { data, loading, error } = useQuery(PAGINATION_QUERY);
+
+  const count = data?._allProductsMeta.count;
+  const pageCount = Math.ceil(count / perPage);
+
+  if (loading) return "Loading...";
+  if (error) return <DisplayError error={error} />;
+
+  return (
+    <PaginationStyles>
+      <Head>
+        <title>Sick Fits - Page {page} of {pageCount}</title>
+      </Head>
+      <Link href={`/products/${page - 1}`}>
+        <a aria-disabled={page === 1}>&larr; Prev</a>
+      </Link>
+      <p>Page {page} of {pageCount}</p>
+      <p>{count} Items Total</p>
+      <Link href={`/products/${page + 1}`}>
+        <a aria-disabled={page === pageCount}>Next &rarr;</a>
+      </Link>
+    </PaginationStyles>
+  );
+};
--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,9 +1,12 @@
+import { Pagination } from "../components/Pagination/Pagination";
 import { Products } from "../components/Products/Products"
 
 const ProductsPage = () => {
   return (
   <div>
+    <Pagination page={1} />
     <Products />
+    <Pagination page={1} />
   </div>
   );
 };
